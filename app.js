acd6cb4eb81eecc027302952e8055d34cb7573c7
guptatwinkal95-ux <guptatwinkal95@gmail.com>
2025-11-24 14:05:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const student = {
+    name: "Twinkal",
+    age: 21;
+    class: 9,
+    subjects: ["hindi", "english", "math", "science"]
+    username: "Twinkal@123",
+    password: "abcd"
+};
+
+let username = student.username;
+let password = student.password;
+console.log(user);
